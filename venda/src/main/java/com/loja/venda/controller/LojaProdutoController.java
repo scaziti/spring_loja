36daecebae93bccd8a8b94071ac2cf57818a3d61
Raphael Scaziti <rscaziti@gmail.com>
2021-03-01 16:20:29 -0300

    feat(produto/controller): adicionado controller, além do método GetAll

---
+++ venda/src/main/java/com/loja/venda/controller/LojaProdutoController.java
@@ -0,0 +1,28 @@
+package com.loja.venda.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.loja.venda.model.LojaProdutoModel;
+import com.loja.venda.repository.LojaProdutoRepository;
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class LojaProdutoController 
+{
+	@Autowired
+	private LojaProdutoRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<LojaProdutoModel>> GetAll()
+	{
+		return ResponseEntity.ok(this.repository.findAll());
+	}
+}
