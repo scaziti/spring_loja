8565ea03b83280f11210bd699466b545ad755673
Raphael Scaziti <rscaziti@gmail.com>
2021-03-01 16:13:21 -0300

    feat(produto/model): adicionado model, inlcuido atributos id, nome, descricao, preco, estoque e ativo

---
+++ venda/src/main/java/com/loja/venda/model/LojaProdutoModel.java
@@ -0,0 +1,84 @@
+package com.loja.venda.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class LojaProdutoModel 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idProduto;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String nomeProduto;
+	
+	@NotNull
+	@Size(min = 10, max = 300)
+	private String descricaoProduto;
+	
+	@NotNull
+	private Double preco;
+	
+	@NotNull
+	private Integer estoque;
+	
+	@NotNull
+	private Boolean ativo;
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
