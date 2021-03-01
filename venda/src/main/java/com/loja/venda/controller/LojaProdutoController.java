465d8807adcd149ccec266da57addee0f21cdfb3
Raphael Scaziti <rscaziti@gmail.com>
2021-03-01 16:30:08 -0300

    feat(produto/controller): adicionado método PUT

--- venda/src/main/java/com/loja/venda/controller/LojaProdutoController.java
+++ venda/src/main/java/com/loja/venda/controller/LojaProdutoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +49,11 @@ public class LojaProdutoController
 	public ResponseEntity<LojaProdutoModel> post(@RequestBody LojaProdutoModel produto)
 	{
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(produto));
+	}
+	
+	@PutMapping
+	public ResponseEntity<LojaProdutoModel> put(@RequestBody LojaProdutoModel produto)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(this.repository.save(produto));
 	}
 }
