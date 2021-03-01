6dd7b4b488854f07fb0c8048b557445ed00a91ce
Raphael Scaziti <rscaziti@gmail.com>
2021-03-01 15:48:07 -0300

    feat(categoria/controller): adicionado método GetByNome

--- venda/src/main/java/com/loja/venda/controller/LojaCategoriaController.java
+++ venda/src/main/java/com/loja/venda/controller/LojaCategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class LojaCategoriaController
 	public ResponseEntity<List<LojaCategoriaModel>> GetAll()
 	{
 		return ResponseEntity.ok(this.repository.findAll());
+	}
+	
+	@GetMapping("/nome/{nomeCategoria}")
+	public ResponseEntity<List<LojaCategoriaModel>> GetByNome(@PathVariable String nomeCategoria)
+	{
+		return ResponseEntity.ok(this.repository.findAllByNomeCategoriaContainingIgnoreCase(nomeCategoria));
 	}
 }
