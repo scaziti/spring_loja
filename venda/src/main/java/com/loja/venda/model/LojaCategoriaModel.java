73be6ed7e0019514c1437f6e0609ef372abd0727
Raphael Scaziti <rscaziti@gmail.com>
2021-03-01 15:26:19 -0300

    feat(categoria/model): adicionado model com id, nome, ativo

---
+++ venda/src/main/java/com/loja/venda/model/LojaCategoriaModel.java
@@ -0,0 +1,51 @@
+package com.loja.venda.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class LojaCategoriaModel 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCategoria;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String nomeCategoria;
+	
+	@NotNull
+	private boolean ativo;
+
+	public Long getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(Long idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+}
