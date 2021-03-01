2b85183dbdb8a04ae7e9a9e1ab0f5e0a39637ee2
Raphael Scaziti <rscaziti@gmail.com>
2021-03-01 15:14:52 -0300

    feat(usuario/controller): adicionado método POST

--- venda/src/main/java/com/loja/venda/controller/LojaUsuarioController.java
+++ venda/src/main/java/com/loja/venda/controller/LojaUsuarioController.java
@@ -3,10 +3,13 @@ package com.loja.venda.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +42,11 @@ public class LojaUsuarioController
 		return this.repository.findById(idUsuario)
 				.map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<LojaUsuarioModel> post(@RequestBody LojaUsuarioModel usuario)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(usuario));
 	}
 }
