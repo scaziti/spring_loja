221cdf8745e86256057b58affff384656431f892
Raphael Scaziti <rscaziti@gmail.com>
2021-03-01 15:40:23 -0300

    refactor(usuario/repository): adicionado comentário do Spring para repository

--- venda/src/main/java/com/loja/venda/repository/LojaUsuarioRepository.java
+++ venda/src/main/java/com/loja/venda/repository/LojaUsuarioRepository.java
@@ -2,8 +2,10 @@ package com.loja.venda.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.loja.venda.model.LojaUsuarioModel;
 
+@Repository
 public interface LojaUsuarioRepository extends JpaRepository<LojaUsuarioModel, Long>
 {
 	public List<LojaUsuarioModel> findAllByNomeUsuarioContainingIgnoreCase(String nomeUsuario);
